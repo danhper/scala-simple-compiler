1d6f4006b61adae43fb6fce40d42ba4ed7730788
Daniel Perez <tuvistavie@gmail.com>
2012-05-28 15:24:43 +0900

    Add tmp file.

--- Lexer.hs
+++ Lexer.hs
@@ -26,24 +26,36 @@ data Operator = Add
               | Pow
              
 getToken :: String -> (Token, String)
-getToken [] = Eof
-getToken str@(head:tail) 
-  | C.isDigit head = parseNumber str
-  | C.isAlpha head = getString str
-  | head `elem` "+-*/^%" = getOperator str
-  | otherwise = (Symb head, tail)
+getToken [] = (Eof, [])
+getToken str@(x:xs) 
+  | C.isDigit x = parseNumber str
+  | C.isAlpha x = getString str
+  | x `elem` "+-*/^%" = getOperator str
+  | otherwise = (Symb x, xs)
                                        
 parseNumber :: String -> (Token, String)
 parseNumber str = parse str 0 where
-  parse [] _ = error "Missing operand"
-  parse str@(head:tail) number 
-    | C.isDigit head = parse tail (number * 10 + (digitToInt head))
-    | head == "." = let (fractPart, leftString) = parseRealPart tail 
-                    in (DoubleType number + fractPart, leftString)  
+  parse [] number = (IntType number, [])
+  parse str@(x:xs) number 
+    | C.isDigit x = parse xs (number * 10 + (digitToInt x))
+    | x == "." = let (fractPart, leftString) = parseRealPart xs 
+                    in (DoubleType number + fractPart, leftString)
     | otherwise = (IntType number, str)
 
 parseRealPart :: String -> (Double, String)
 parseRealPart str = parse str 0.0 10 where
   parse [] _ _ = 0.0
-  parse (head:tail) number divider
-    | C.isDigit head = parse tail (number / divider) (divider * 10)
+  parse str@(x:xs) number divider
+    | C.isDigit x = parse xs (number + (digitToInt x) / divider) (divider * 10)
+    | otherwise = (number, str)
+                  
+getString :: String -> (Token, String)
+getString str = parse str "" where
+  parse [] word = (StringType word, [])
+  parse str@(x:xs) word
+    | !(C.isSpace x) = parse xs (word ++ [x])
+    | otherwise = (StringType word, str)
+                  
+getOperator :: String -> (Token, String)
+
+  
